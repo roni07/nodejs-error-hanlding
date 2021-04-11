29e434306c8be37a4f15095d2b5741dceeab08f5
roni07 <mdmehedihasanroni28@gmail.com>
2021-04-11 22:26:16 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const morgan = require('morgan');
+const express = require('express');
+const tour = require('./controllers/tour-controller');
+const AppError = require('./utils/app-error')
+const globalErrorHandler = require('./error-handler/global-error-handler');
+
+require('./config/db-connection')();
+
+module.exports = function (app) {
+    app.use(express.json());
+    app.use(morgan('dev'));
+
+    app.use('/api/tour', tour);
+
+    app.all('*', (req, res, next) => {
+        next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+    });
+
+    app.use(globalErrorHandler)
+}
